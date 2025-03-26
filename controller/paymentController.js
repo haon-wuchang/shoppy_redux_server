7fb9bd737eaa4e5b552234e18c3dfce0489de589
haon <haoony@naver.com>
2025-03-26 11:51:46 +0900

    d

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -20,9 +20,9 @@ export const payment = async(req, res) => {
                 quantity: 1,
                 total_amount,
                 tax_free_amount: 0,
-                approval_url: "http://13.125.159.44:3000/payment/success",
-                cancel_url: "http://13.125.159.44:3000/payment/cancel",
-                fail_url: "http://13.125.159.44:3000/payment/fail",
+                approval_url: "http://shoppy-redux-react.s3-website.ap-northeast-2.amazonaws.com//payment/success",
+                cancel_url: "http://shoppy-redux-react.s3-website.ap-northeast-2.amazonaws.com//payment/cancel",
+                fail_url: "http://shoppy-redux-react.s3-website.ap-northeast-2.amazonaws.com//payment/fail",
             },
             {
                 headers: {
